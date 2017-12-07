4f4e596f831af21b1a107730ed24babb4183f9b9
Kemal Demirtas <demirtas@kth.se>
2017-12-07 13:42:33 +0100

    FAQ

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,11 +3,11 @@ $('.container div').click(function() {
     if ($(this).children(".text").hasClass("hidden")) {
         $(this).children(".text").slideDown();
         $(this).children(".text").removeClass("hidden");
-        $(this).children(".info").slideUp();
+        $(this).children(".info").fadeToggle();
     } else {
         $(this).children(".text").slideUp();
         $(this).children(".text").addClass("hidden");
-        $(this).children(".info").slideDown();
+        $(this).children(".info").fadeToggle();
     }
 
 });
