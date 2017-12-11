0f65556f812f27454e6139c3318af66fc46fef48
Kemal Demirtas <demirtas@kth.se>
2017-12-11 15:24:55 +0100

    made FAQ slidey

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -10,10 +10,18 @@ $("window").ready(function () {
         $("#countdown").fadeToggle(3500, "swing");
     });
 
-    $(".text").slideToggle();
+    //$(".text").slideToggle();
 });
 
 
 $("div#loginbutton > button").click(function () {
     $('div#login').fadeToggle(300);
+});
+
+$('main#faq > div').click(function () {
+    $(this).next().slideToggle();
+});
+
+$('main#faq > div > h3').click(function () {
+    $(this).next().slideToggle();
 });
