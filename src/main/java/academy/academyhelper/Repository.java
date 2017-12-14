46c5f5d018fab08afaf3a680d255e845ee141370
Kemal Demirtas <demirtas@kth.se>
2017-12-14 17:47:14 +0100

    haha

--- src/main/java/academy/academyhelper/Repository.java
+++ src/main/java/academy/academyhelper/Repository.java
@@ -504,17 +504,10 @@ public class Repository {
                 request.setMethod(Method.POST);
                 request.setEndpoint("mail/send");
                 request.setBody(mail.build());
-
-                Response response = sg.api(request);
-
-                System.out.println(response.getStatusCode());
-                System.out.println(response.getBody());
-                System.out.println(response.getHeaders());
-
+                
             } catch (IOException ex) {
                 throw ex;
             }
-
         }
     }
 }
